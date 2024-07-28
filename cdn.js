c0ef0e60d853a5b023152bbd87908688c0433b75
Code black spider <91369752+Codeblackspider@users.noreply.github.com>
2024-07-28 22:06:13 +0530

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -2,20 +2,44 @@
 const adContents = [
     {
         imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh1CwFt7Uja8CGSXKzhvZjo4AiDtTyHoD0Z-BYs2zKKE1SjuZSgBPTE4FswOnH2qlynrExjof3IsANDw52tQJsfZrqK3OCQJaC0QW82ldwgy98pq_Fe39FZkX2PczAvU3jjU1HPB_0cKKShRu4h9_DRxoaDLaLM9kREdSVIQOKSllmjcZ_c30ut8lGwQNJd/w640-h360/photo_2024-07-27_16-02-54.jpg",
+        text: "Ethical Hacking - Basics (Kali 2021)",
+        link: "https://www.fontbees.store/2024/07/ethical-hacking-basics-kali-2021.html"
+    },
+    
+    {
+        imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjVWLERcM_pvfyj8ww31tQKrUylPlR6j0OZeG3FB3RaEaDJHckZt9PfGVxtpLkb5JKR5NDYKI_bIvw2vnzWQEiFq9ewgV5rzZSas781CdIYUGZMBTK0hJxB_xPlMLv-SqaMfj4Wa8XEwmaAjVITJQT4a6LS56wtFohyphenhyphen1HEhYVW_Ey5xE-cuEtioCr_uWDem/w640-h360/photo_2024-07-27_16-50-27.jpg",
+        text: "Free Udemy Nmap Course: Master Ethical Hacking & Network Security",
+        link: "https://www.fontbees.store/2024/07/master-nmap-ethical-hacking-network-security.html"
+    },
+
+    {
+        imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh1CwFt7Uja8CGSXKzhvZjo4AiDtTyHoD0Z-BYs2zKKE1SjuZSgBPTE4FswOnH2qlynrExjof3IsANDw52tQJsfZrqK3OCQJaC0QW82ldwgy98pq_Fe39FZkX2PczAvU3jjU1HPB_0cKKShRu4h9_DRxoaDLaLM9kREdSVIQOKSllmjcZ_c30ut8lGwQNJd/w640-h360/photo_2024-07-27_16-02-54.jpg",
+        text: "Master Ethical Hacking: Free Udemy Course on Password Cracking and Cyber Security",
+        link: "https://www.fontbees.store/2024/07/master-ethical-hacking-free-udemy-course.html"
+    },
+    {
+        imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgUs_HS7pL4gokAA5ewSisXwh5jZK0ao3MjSe_s54Bs0jSyMXatMOUmJbq_KK4jsMa6vTIfeJhesp3EAxNWzH3fgCt1pp7yXFGFj7jueqdNydjIe6PzsMhDUwa_RJODwIXWllthzBehnLn_DnNSYnNfvjtPZsOS5Nd9P5KiDOVyU-3CK9QvKzn_e941va1j/w640-h360/photo_2024-07-27_17-01-36.jpg",
+        text: "Free Course: Learn RATs and Crypters in Ethical Hacking",
+        link: "https://www.fontbees.store/2024/07/learn-rats-crypters-ethical-hacking.html"
+    },
+
+// Add more ad objects as needed
+
+/*
+    {
+        imageUrl: "https://image1.url",
         text: "Blog 1",
         link: "https://blog1.url"
     },
     {
-        imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi_RG0IyTzS-wJSJJxlHAKn8Kn6OP2jO6VOVswot3OAXg47YOKyUHjG39Ni5XQQA-qtMqmuFt3bvZPiUAiMf-g03GzDW5LQUy87jErc5S6avow1gb92_ZMtg9s5TbLqadqP5czMLa9G3HBHCoxNOBmYE_NtO8p1gOEpn-FZUgDPvHBXT8rp_R9JIBQCVGxs/s750/photo_2024-07-27_17-51-17.jpg",
+        imageUrl: "https://image2.url",
         text: "Blog 2",
         link: "https://blog2.url"
     },
-    {
-        imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjVWLERcM_pvfyj8ww31tQKrUylPlR6j0OZeG3FB3RaEaDJHckZt9PfGVxtpLkb5JKR5NDYKI_bIvw2vnzWQEiFq9ewgV5rzZSas781CdIYUGZMBTK0hJxB_xPlMLv-SqaMfj4Wa8XEwmaAjVITJQT4a6LS56wtFohyphenhyphen1HEhYVW_Ey5xE-cuEtioCr_uWDem/w640-h360/photo_2024-07-27_16-50-27.jpg",
-        text: "Blog 2",
-        link: "https://blog2.url"
-    },
-    // Add more ad objects as needed
+    
+*/
+
+
 ];
 
 function closePopupAd() {
